00d80e77799fe8c770780594a539aa14753bb7c5
Davide_24 <009davide@gmail.com>
2024-09-17 06:42:43 +0200

    Added minimum movement joystick movement amount.

--- WSHRCCarController/MainPage.xaml.cs
+++ WSHRCCarController/MainPage.xaml.cs
@@ -62,16 +62,36 @@ namespace WSHRCCarController
             Joystick_Stick.TranslationY = rawTranslationY;
             Joystick_Stick.TranslationX = rawTranslationX;
             // Set Motor steer angle (Round value correctly)
-            MotorSpeed = (int)Math.Round(actualMotorSpeed);
-            MotorSteerAngle = (int)Math.Round(actualMotorSteer);
 
-            Debug.Write($"MotorSpeed: {MotorSpeed}; RAW: {e.TotalY} ||||");
-            Debug.WriteLine($"MotorSteer: {MotorSteerAngle}; RAW: {e.TotalX}");
+            int previousMotorSpeed = MotorSpeed;
+            int previousMotorSteer = MotorSteerAngle;
+
+            if ((previousMotorSpeed != (int)Math.Round(actualMotorSpeed) && System.Math.Abs(previousMotorSpeed - (int)Math.Round(actualMotorSpeed)) > 5) || (int)Math.Round(actualMotorSpeed) == 0)
+            {
+                MotorSpeed = (int)Math.Round(actualMotorSpeed);
+            }
+
+            if ((previousMotorSteer != (int)Math.Round(actualMotorSteer) && System.Math.Abs(previousMotorSteer - (int)Math.Round(actualMotorSteer)) > 5) || (int)Math.Round(actualMotorSteer) == 0)
+            {
+                MotorSteerAngle = (int)Math.Round(actualMotorSteer);
+            }
+
+            //Debug.Write($"MotorSpeed: {MotorSpeed}; RAW: {e.TotalY} ||||");
+            //Debug.WriteLine($"MotorSteer: {MotorSteerAngle}; RAW: {e.TotalX}");
 
             // Send the data to the car
             Services.IBluetoothService bluetoothService = DependencyService.Get<Services.IBluetoothService>();
-            bluetoothService.SendData(new Services.RCData { Type = Services.RCDataType.Speed, value = MotorSpeed });
-            bluetoothService.SendData(new Services.RCData { Type = Services.RCDataType.Steer, value = MotorSteerAngle });
+            if (previousMotorSpeed != MotorSpeed || MotorSpeed == 0)
+            {
+                Debug.Write($"MotorSpeed: {MotorSpeed}; RAW: {e.TotalY} ||||");
+                bluetoothService.SendData(new Services.RCData { Type = Services.RCDataType.Speed, value = MotorSpeed });
+            }
+
+            if (previousMotorSteer != MotorSteerAngle || MotorSteerAngle == 0)
+            {
+                bluetoothService.SendData(new Services.RCData { Type = Services.RCDataType.Steer, value = MotorSteerAngle });
+                Debug.WriteLine($"MotorSteer: {MotorSteerAngle}; RAW: {e.TotalX}");
+            }
         }
     }
 
